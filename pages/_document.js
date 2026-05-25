e35557d4352b11d08e0309efe964dc2f771cd013
nilsholthey <holthey@foxxum.com>
2026-05-25 23:28:56 +0200

    generate and add images

--- pages/_document.js
+++ pages/_document.js
@@ -19,8 +19,6 @@ export default function Document() {
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Tippspiel" />
         <link rel="apple-touch-icon" href="/icons/icon-512.png" />
-
-        {/* iOS splash screens — generated via pwa-asset-generator */}
         <link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="/splash/apple-splash-2048-2732.png" />
         <link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="/splash/apple-splash-1668-2388.png" />
         <link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="/splash/apple-splash-1536-2048.png" />
