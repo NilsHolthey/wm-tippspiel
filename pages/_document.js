8a85dc2b6fdcd900595350eabf87ed4548ba44fd
nilsholthey <holthey@foxxum.com>
2026-05-25 19:03:52 +0200

    add pwa

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,6 @@ export default function Document() {
         <link rel="icon" href="/icons/icon-192.png" type="image/png" sizes="192x192" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#C9A84C" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Tippspiel" />
